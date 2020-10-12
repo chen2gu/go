0e5101e3dd91e2a731310e24ee6738e2532a1914
Chen Bin <chen2guo@foxmail.com>
2020-10-12 14:41:55 +0800

    add day12

---
+++ day12/01map/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var userinfo = make(map[string]string)
+
+	userinfo["username"] = "张三"
+	userinfo["age"] = "20"
+	userinfo["sex"] = "男"
+
+	fmt.Println(userinfo)
+
+	fmt.Println(userinfo["username"])
+}
