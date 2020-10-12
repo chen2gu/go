58df568bc14b0560cf6d5ae4cb6fabf9ca74227b
Chen Bin <chen2guo@foxmail.com>
2020-10-12 14:49:55 +0800

    add day12

--- day12/01map/main.go
+++ day12/01map/main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+
+	fmt.Println("one")
 	var userinfo = make(map[string]string)
 
 	userinfo["username"] = "张三"
@@ -12,4 +14,15 @@ func main() {
 	fmt.Println(userinfo)
 
 	fmt.Println(userinfo["username"])
+
+	fmt.Println("two")
+	userinfo2 := map[string]string{
+		"username": "李四",
+		"age":      "21",
+		"sex":      "男",
+	}
+
+	fmt.Println(userinfo2)
+
+	fmt.Println(userinfo2["username"])
 }
