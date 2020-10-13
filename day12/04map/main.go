b9bf7033fbc8704f3b9f39c6af1884ae1c8c7bc4
Chen Bin <chen2guo@foxmail.com>
2020-10-13 17:17:55 +0800

    update map

---
+++ day12/04map/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	userinfo := make([]map[string]string, 3, 3)
+	//fmt.Println(userinfo[0] == nil)
+
+	if userinfo[0] == nil {
+		userinfo[0] = make(map[string]string)
+
+		userinfo[0]["username"] = "张三"
+		userinfo[0]["age"] = "20"
+		userinfo[0]["hight"] = "170cm"
+	}
+
+	if userinfo[1] == nil {
+		userinfo[1] = make(map[string]string)
+
+		userinfo[1]["username"] = "李四"
+		userinfo[1]["age"] = "21"
+		userinfo[1]["hight"] = "180cm"
+	}
+
+	fmt.Println(userinfo)
+
+}
