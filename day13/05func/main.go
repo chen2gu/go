cabd797785f84d5c6ebd3108154bf0f5e33229d3
Chen Bin <chen2guo@foxmail.com>
2020-10-21 15:54:19 +0800

    add var

---
+++ day13/05func/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+var a = "a初始值: 全局变量"
+
+func run() {
+	var a = "a初始值: 局部变量"
+	var b = "b初始值: 局部变量"
+	fmt.Println(a)
+	fmt.Println(b)
+	return
+}
+
+func main() {
+
+	fmt.Println(a)
+	run()
+	fmt.Println(a)
+
+	for i := 1; i < 11; i++ {
+		fmt.Println(i)
+	}
+
+}
