0a056c6c3056ab977890316eb68bceabc6ff3155
Chen Bin <chen2guo@foxmail.com>
2020-10-21 15:09:08 +0800

    add -->

--- day13/03func/main.go
+++ day13/03func/main.go
@@ -16,10 +16,29 @@ func sortInAsc(slice []int) []int {
 
 }
 
+// int 降序排序
+func sortInDesc(slice []int) []int {
+	//
+	for i := 0; i < len(slice); i++ {
+		for j := i + 1; j < len(slice); j++ {
+			if slice[i] < slice[j] {
+				slice[i], slice[j] = slice[j], slice[i]
+			}
+		}
+	}
+	return slice
+
+}
+
 func main() {
 
 	slinA := []int{12, 33, 1, 56, 85, 22, 19, 11, 1, 0, -1}
+	fmt.Println("升序--->")
 	arr := sortInAsc(slinA)
 	fmt.Println(arr)
+
+	fmt.Println("降序-->")
+	desc := sortInDesc(slinA)
+	fmt.Println(desc)
 	fmt.Println("Ok ?")
 }
